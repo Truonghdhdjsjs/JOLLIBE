7005efba1ca9d35f265bfbdeadc0b0eccd2860d9
Truonghdhdjsjs <locmayman0702@gmail.com>
2023-11-25 17:59:32 +0700

    m

--- main/product.php
+++ main/product.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JOLLIBEE</title>
-   <link rel="stylesheet" href="css/trangchu.css">
-   <link rel="stylesheet" href="css/default.css">
-   <link rel="stylesheet" href="css/color.css">
-   <link rel="stylesheet" href="css/sanpham.css">
-   <link rel="website icon" type="png" href="img/logo/Jollibee-logo.png">
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-<body>
+<?php
+    include("../main/origin/header.php");
+?>
+<?php
+include("../main/origin/nav.php");
+?>
     <div class="app">
         <header id="header">
             <header id="header-top">
@@ -250,7 +241,6 @@
             </section>
         </div>
     </div>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-</html>
+<?php
+    include("../main/origin/footer.php");
+?>
